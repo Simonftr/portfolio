d201d48f699de5daa5b73c5c79e975117349b552
Simonftr <simon.fetre@etu.univ-smb.fr>
2024-12-02 23:28:55 +0100

    Pas fou

--- main.js
+++ main.js
@@ -16,6 +16,50 @@ let homeButton = document.querySelector("#homeButton").addEventListener("click",
 
 })
 
-projet = {
-    'titre': "test"
+
+
+const projetsBox = document.querySelector("#projetsBox")
+
+projet1 = {
+    'titre': "projet1",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue."
 }
+projet2 = {
+    'titre': "projet2",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue."
+}
+projet3 = {
+    'titre': "projet3",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue."
+}
+projet4 = {
+    'titre': "projet4",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue."
+}
+projet5 = {
+    'titre': "projet5",
+    'description': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue."
+}
+
+
+projets = []
+projets.push(projet1)
+projets.push(projet2)
+projets.push(projet3)
+projets.push(projet4)
+projets.push(projet5)
+
+projets.forEach(projet => {
+    let projetDiv = document.createElement("li")
+    let photo = document.createElement("img")
+    let detailsDiv = document.createElement("div")
+    projetDiv.classList.add("projet")
+    detailsDiv.classList.add("detailsProjet")
+    projetDiv.innerHTML = projet["titre"]
+    projetsBox.appendChild(projetDiv)
+    projetDiv.appendChild(photo)
+    projetDiv.appendChild(detailsDiv)
+});
+
+
+
