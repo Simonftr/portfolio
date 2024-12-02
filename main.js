4e69eee0a5f3a48cd89f6f98ea52ad2d271e9b72
Simonftr <155958313+Simonftr@users.noreply.github.com>
2024-12-02 12:57:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let background = document.querySelector("#background")
+document.addEventListener("mousemove", event => {
+    background.style.background = "radial-gradient(circle at "+event.clientX+"px "+event.clientY+"px,rgb(200, 0, 255,0.25),rgb(25, 25, 25)";
+}
+)
+/* 
+let cursor = document.querySelector("#cursor")
+document.addEventListener("mousemove", event => {
+    cursor.style.top = event.clientY+"px";
+    cursor.style.left = event.clientX+"px";
+}
+) */
+
+let homeButton = document.querySelector("#homeButton").addEventListener("click", event => {
+    window.location.href = 'index.html'; 
+
+})
